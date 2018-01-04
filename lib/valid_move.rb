204c3b2bb9fb6a790dbbe99bba73c586b9ea2a1e
Dalton Fant <daltonfant16@gmail.com>
2018-01-04 20:05:06 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -3,14 +3,10 @@ number_entered = number_entered.to_i - 1
   def valid_move?(number_entered)
   if number_entered.between?(0, 8)
     true
-  else position_taken?
-    false
   end
 end
 
 
-
-board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
 def position_taken?(board, index)
   if board[index] == "" || board[index] == " " || board[index] == nil
