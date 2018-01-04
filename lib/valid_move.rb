fdc4831a47be87717861bc8adb7fb3d60db72437
Dalton Fant <daltonfant16@gmail.com>
2018-01-04 19:51:18 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,5 +1,5 @@
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-
+number_entered = number_entered.to_i - 1
   def valid_move?(number_entered, board)
   if number_entered.between?(0, 8) && !(position_taken?(board, number_entered))
     true
