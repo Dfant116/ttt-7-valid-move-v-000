16c348701a2d10f5008411017aaf8f50b2b62622
Dalton Fant <daltonfant16@gmail.com>
2018-01-04 19:18:13 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,11 +1,11 @@
 def valid_move?(board, index)
-  if board[index].between(0, 8)
+  if board[index].between?(0, 8)
     true
   end
 end
 
-
-
+    
+    
 
 
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
