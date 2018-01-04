f0382cd42f94096e62fdb72ba1ca04e139d93073
Dalton Fant <daltonfant16@gmail.com>
2018-01-04 19:48:58 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,16 +1,11 @@
 board = [" ", " ", "X", " ", " ", " ", " ", " ", " "]
-number_entered = number_entered.to_i - 1
-def valid_move?(number_entered, board)
+user_input = gets.chomp 
+number_entered = user_input.to_i - 1
+  def valid_move?(number_entered, board)
   if number_entered.between?(0, 8) && !(position_taken?(board, number_entered))
     true
   end
 end
-
-
-
-
-
-
 
 
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
@@ -22,3 +17,4 @@ def position_taken?(board, index)
     true
   end
 end
+
